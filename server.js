a965df397cc6c03c33aaa78a76a4e2811c7c5fb4
Olena1010 <ebojcenko55@gmail.com>
2023-08-18 18:37:28 +0300

    add libraries

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
+const mongoose = require('mongoose');
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+
+const DB_HOST = "mongodb+srv://Olena:WrP8c9nbLC7EmwFx@cluster0.82qjtoe.mongodb.net/db-contacts?retryWrites=true&w=majority"
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000)
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
+// WrP8c9nbLC7EmwFx
+// const DB_HOST = "mongodb+srv://Olena:WrP8c9nbLC7EmwFx@cluster0.82qjtoe.mongodb.net/db-contacts?retryWrites=true&w=majority"
