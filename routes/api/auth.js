2b4978df5d59bd6536a84ac805c226a924007736
Olena1010 <ebojcenko55@gmail.com>
2023-08-31 13:01:29 +0300

    part of the task completed

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const ctrl = require("../../controllers/auth")
 
-const { validateBody, authenticate } = require('../../middlewares');
+const { validateBody, authenticate, upload } = require('../../middlewares');
 
 const { schemas } = require("../../models/user");
 const { ctrlWrapper } = require("../../helpers");
@@ -21,5 +21,10 @@ router.patch(
   validateBody(updateSubscribeSchema),
   ctrl.changeSubscription,
 );
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrl.updateAvatar); 
 
-module.exports = router;
+module.exports = router;         
